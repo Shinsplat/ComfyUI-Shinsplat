22c2664ebe809e277c569c60d5580fde5ed8b241
Shinsplat Tarterbox <90067363+Shinsplat@users.noreply.github.com>
2024-06-08 16:20:59 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -5,11 +5,8 @@ from .shinsplat_clip_text_encode_sdxl import *
 from .shinsplat_lora_loader import *
 from .shinsplat_sum_wrap import *
 from .shinsplat_green_box import *
-#from .shinsplat_python import NODE_CLASS_MAPPINGS
-#from .shinsplat_python import NODE_DISPLAY_NAME_MAPPINGS
-
 from .shinsplat_python import *
-from .shinsplat_python import *
+from .shinsplat_python_more import *
 
 # --------------------------------------------------------------------------------
 #
@@ -21,6 +18,7 @@ NODE_CLASS_MAPPINGS = {
     "Sum Wrap (Shinsplat)": Shinsplat_SumWrap,
     "Green Box (Shinsplat)": Shinsplat_GreenBox,
     "Python (Shinsplat)": Shinsplat_Python,
+    "Python - More Inputs (Shinsplat)": Shinsplat_PythonMore,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Clip Text Encode (Shinsplat)": "Clip Text Encode (Shinsplat)",
@@ -28,6 +26,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Lora Loader (Shinsplat)": "Lora Loader (Shinsplat)",
     "Green Box (Shinsplat)": "Green Box (Shinsplat)",
     "Python (Shinsplat)": "Python (Shinsplat)",
+    "Python - More Inputs (Shinsplat)": "Python - More Inputs (Shinsplat)",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
